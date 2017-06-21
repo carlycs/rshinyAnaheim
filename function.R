1faf0eba4ea519d42fe0563ccb6b791d31a24c52
Hoofar Pourzand <hp@lotustech.io>
2017-06-21 13:27:22 -0400

    function.R
    wrapper for methods and dummy data used and to also test the ui.R and server.R

---
+++ function.R
@@ -0,0 +1,8 @@
+#function.R wrapper function for getting and reciving data from Sercver.R 
+#some dummy data to start the develepement cycle on ui.R and server.R
+#coming...
+
+#recive a date and time from server.R 
+
+
+#send back the following values to the server.R for rendering
