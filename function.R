1d09d36405bd78d967e6532f1d86d0bd5eeef318
Hoofar Pourzand <hp@lotustech.io>
2017-06-21 13:43:33 -0400

    update function with df and csv

--- function.R
+++ function.R
@@ -1,8 +1,13 @@
 #function.R wrapper function for getting and reciving data from Sercver.R 
 #some dummy data to start the develepement cycle on ui.R and server.R
-#coming...
+#everyline stores data for one hour
+serverdata <- read.csv("../anaheiminput/data.csv")
+serverdata$datetimehourly <- as.POSIXlt(test$datetimehourly, tz="GMT",format="%m/%d/%y %H:%M ")
+
+
 
 #recive a date and time from server.R 
 
 
 #send back the following values to the server.R for rendering
+
