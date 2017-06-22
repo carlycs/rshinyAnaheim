d4a76f4a69e806ab53574b122888087517eaa322
jcdepth <jcdepth@gmail.com>
2017-06-22 02:27:59 -0400

    Working extraction of date from imported CSV columns

--- global.R
+++ global.R
@@ -11,7 +11,10 @@ library(lubridate)
 #some dummy data to start the develepement cycle on ui.R and server.R
 #everyline stores data for one hour
 serverdata <- read_csv("data.csv")
-serverdata$datetimehourly <- mdy_hm(serverdata$datetimehourly, tz="UTC")
+serverdata$datetimehourly <- mdy_hm(serverdata$datetimehourly, tz = "UTC")
+serverdata$yearMonthDay <- format(serverdata$datetimehourly, format = "%Y-%m-%d")
+
+
 
 set_time <- function() {
   this_hour <- hour(Sys.time())
