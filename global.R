1902edd3fc4d898040d75378e48ab400e0bf7fe1
bob_kubinec <rmk7xy@virginia.edu>
2017-06-21 22:03:20 -0400

    up to date shiny function with working google esque graph

--- global.R
+++ global.R
@@ -13,7 +13,18 @@ library(lubridate)
 serverdata <- read_csv("data.csv")
 serverdata$datetimehourly <- mdy_hm(serverdata$datetimehourly, tz="UTC")
 
-
+set_time <- function() {
+  this_hour <- hour(Sys.time())
+  if(this_hour<21 && this_hour>2) {
+    return(c(this_hour-3,this_hour+3))
+  } else {
+    if(this_hour>12) {
+      return(c(this_hour-6,this_hour))
+    } else {
+      return(c(this_hour,this_hour+6))
+    }
+  }
+}
 
 
 
